b0b8350d56cd3c83f6948487714d5a158348aafd
pandeyvijay07-blip <pandeyvijay07@gmail.com>
2026-07-21 19:37:27 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Customers from "./Customers";
 import Products from "./Products";
 import Purchases from "./Purchases";
+import Sales from "./Sales";
 
 function Dashboard() {
   const [page, setPage] = useState("dashboard");
@@ -9,39 +10,28 @@ function Dashboard() {
   if (page === "customers") return <Customers />;
   if (page === "products") return <Products />;
   if (page === "purchases") return <Purchases />;
+  if (page === "sales") return <Sales />;
 
   return (
     <div style={{ padding: 20 }}>
       <h1>MANVI ERP</h1>
 
-      <button onClick={() => setPage("customers")}>
-        Customers
-      </button>
-
+      <button onClick={() => setPage("customers")}>Customers</button>
       <br /><br />
 
-      <button onClick={() => setPage("products")}>
-        Products
-      </button>
-
+      <button onClick={() => setPage("products")}>Products</button>
       <br /><br />
 
-      <button onClick={() => setPage("purchases")}>
-        Purchases
-      </button>
-
+      <button onClick={() => setPage("purchases")}>Purchases</button>
       <br /><br />
 
-      <button>Sales</button>
-
+      <button onClick={() => setPage("sales")}>Sales</button>
       <br /><br />
 
       <button>Collections</button>
-
       <br /><br />
 
       <button>Expenses</button>
-
       <br /><br />
 
       <button>Reports</button>
