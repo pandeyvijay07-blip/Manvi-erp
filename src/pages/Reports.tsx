7a6611bd41ec7efe19a27e2bc99ee9337372f8e1
pandeyvijay07-blip <pandeyvijay07@gmail.com>
2026-07-21 19:42:48 +0530

    Create Reports.tsx

---
+++ src/pages/Reports.tsx
@@ -0,0 +1,35 @@
+function Reports() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Reports</h1>
+
+      <button>Daily Sales Report</button>
+
+      <br /><br />
+
+      <button>Purchase Report</button>
+
+      <br /><br />
+
+      <button>Collection Report</button>
+
+      <br /><br />
+
+      <button>Expense Report</button>
+
+      <br /><br />
+
+      <button>Stock Report</button>
+
+      <br /><br />
+
+      <button>Profit & Loss</button>
+
+      <br /><br />
+
+      <button>Customer Ledger</button>
+    </div>
+  );
+}
+
+export default Reports;
