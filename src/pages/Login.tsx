ad75caa100de35aa5455dd491ba0082b664dc3c2
pandeyvijay07-blip <pandeyvijay07@gmail.com>
2026-07-21 19:24:46 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,46 @@
+import { useState } from "react";
+import { supabase } from "../lib/supabase";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("Login successful");
+    }
+  };
+
   return (
     <div style={{ padding: 20 }}>
       <h1>MANVI ERP</h1>
-      <h2>Login</h2>
 
       <input
         type="email"
         placeholder="Email"
-        style={{ display: "block", marginBottom: 10, width: "100%", padding: 10 }}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
+
+      <br /><br />
 
       <input
         type="password"
         placeholder="Password"
-        style={{ display: "block", marginBottom: 10, width: "100%", padding: 10 }}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
-      <button style={{ padding: 10, width: "100%" }}>
+      <br /><br />
+
+      <button onClick={login}>
         Login
       </button>
     </div>
