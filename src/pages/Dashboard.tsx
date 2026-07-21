eec9d9834444422b25b1a08535317f557f29f835
pandeyvijay07-blip <pandeyvijay07@gmail.com>
2026-07-21 19:33:28 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import Customers from "./Customers";
+import Products from "./Products";
 
 function Dashboard() {
   const [page, setPage] = useState("dashboard");
 
   if (page === "customers") {
     return <Customers />;
+  }
+
+  if (page === "products") {
+    return <Products />;
   }
 
   return (
@@ -18,19 +23,26 @@ function Dashboard() {
 
       <br /><br />
 
-      <button>Products</button>
+      <button onClick={() => setPage("products")}>
+        Products
+      </button>
+
       <br /><br />
 
       <button>Purchases</button>
+
       <br /><br />
 
       <button>Sales</button>
+
       <br /><br />
 
       <button>Collections</button>
+
       <br /><br />
 
       <button>Expenses</button>
+
       <br /><br />
 
       <button>Reports</button>
