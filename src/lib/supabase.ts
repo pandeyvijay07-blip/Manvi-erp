3b23166e895990157d400f41afb40f7f08600860
pandeyvijay07-blip <pandeyvijay07@gmail.com>
2026-07-21 19:15:40 +0530

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "";
+const supabaseAnonKey = "";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
