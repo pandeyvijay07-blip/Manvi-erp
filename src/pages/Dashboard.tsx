79684430e4eb4813f9d311e2c9ae9668d76be58d
pandeyvijay07-blip <pandeyvijay07@gmail.com>
2026-07-21 19:25:31 +0530

    Create Dashboard.tsx

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,37 @@
+function Dashboard() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>MANVI ERP Dashboard</h1>
+
+      <div style={{ marginTop: 20 }}>
+        <button>Customers</button>
+      </div>
+
+      <div style={{ marginTop: 10 }}>
+        <button>Products</button>
+      </div>
+
+      <div style={{ marginTop: 10 }}>
+        <button>Purchases</button>
+      </div>
+
+      <div style={{ marginTop: 10 }}>
+        <button>Sales</button>
+      </div>
+
+      <div style={{ marginTop: 10 }}>
+        <button>Collections</button>
+      </div>
+
+      <div style={{ marginTop: 10 }}>
+        <button>Expenses</button>
+      </div>
+
+      <div style={{ marginTop: 10 }}>
+        <button>Reports</button>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
